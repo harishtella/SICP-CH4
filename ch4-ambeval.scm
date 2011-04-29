8a9457eeb5471a7e9b7c4559a4e7618a6595c4fc
Harish <harishtella@gmail.com>
2011-04-29 15:53:09 -0500

    complete EX 4.42

--- ch4-ambeval.scm
+++ ch4-ambeval.scm
@@ -304,6 +304,8 @@
         (list 'memq memq)
         (list 'member member)
         (list 'not not)
+		(list 'xor (lambda (x y) (and (or x y)
+									  (not (and x y)))))
         (list '+ +)
         (list '- -)
         (list '* *)
@@ -396,6 +398,21 @@
 				  (list 'smith smith)))))))))
 
 
+; EX 4.42
+(interpret
+'(define (liars-puzzle)
+  (let ((b (amb 1 2 3 4 5))
+		(e (amb 1 2 3 4 5))
+		(j (amb 1 2 3 4 5))
+		(k (amb 1 2 3 4 5))
+		(m (amb 1 2 3 4 5)))
+	(require (distinct? (list b e j k m)))
+	(require (xor (= k 2) (= b 3)))
+	(require (xor (= e 1) (= j 2)))
+	(require (xor (= j 3) (= e 5)))
+	(require (xor (= k 2) (= m 4)))
+	(require (xor (= m 4) (= b 1)))
+	(list 'b b 'e e 'j j 'k k 'm m))))
 
 
 ;; Startup repl on file load
